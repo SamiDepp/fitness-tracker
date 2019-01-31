08d71182c331769afd0013d9f23a043eb110ef1c
Slimane Deb <cs_deb@esi.dz>
2019-01-31 09:57:36 +0100

    push app

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBJwsXjJVmU3_0g5GSLF9XeneaIwvdlciw",
+    authDomain: "ng-fitness-tracker-836d5.firebaseapp.com",
+    databaseURL: "https://ng-fitness-tracker-836d5.firebaseio.com",
+    projectId: "ng-fitness-tracker-836d5",
+    storageBucket: "ng-fitness-tracker-836d5.appspot.com",
+    messagingSenderId: "253105677115"
+  }
+};
